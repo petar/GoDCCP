6de0c3ff03b55ab80af4d871a26b87fe8289aaa0
Petar Maymounkov <petarm@gmail.com>
2011-09-03 14:32:21 -0400

    ok

--- dccp/ccid3/gentab.go
+++ dccp/ccid3/gentab.go
@@ -9,11 +9,24 @@ import (
 	"math"
 )
 
+var qTable = []struct{ rateInvLo uint32; q int64 }{}
+
 func main() {
+	fmt.Printf(
+`// THIS FILE IS AUTO-GENERATED
+
+package ccid3
+
+var qTable = []struct{ rateInvLo uint32; q int64 }{
+`)
 	// j is the loss rate inverse
 	for j := 1; j <= 100; j++ {
 		p := 1 / float64(j) // loss rate
 		q := (math.Sqrt(2*p/3) + 12*math.Sqrt(3*p/8)*p*(1+32*p*p))
-		fmt.Printf("%d\t%1.uint32\t%1.3g\n", j, p, q)
+		fmt.Printf("\t{ %d, %g\t},\n", j, q)
 	}
+	fmt.Printf(
+`
+}
+`)
 }
