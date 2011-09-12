d17b92178feec9277f95f509d82f7b0cc845c21c
Petar Maymounkov <petar@math.mit.edu>
2011-09-11 21:27:46 -0400

    added windowHistory

--- dccp/ccid3/windowcounter.go
+++ dccp/ccid3/windowcounter.go
@@ -4,11 +4,13 @@
 
 package ccid3
 
+// —————
 // windowCounter maintains the window counter (WC) logic of the sender.
 // It's logic is described in RFC 4342, Section 8.1.
 type windowCounter struct {
 	lastCounter byte   // The last window counter value sent
 	lastTime    int64  // The time at which the first packet with window counter value lastCounter was sent
+	windowHistory
 }
 
 const (
@@ -50,4 +52,64 @@ func (wc *windowCounter) OnRead(rtt int64, wcAckd byte, now int64) {
 		wc.lastCounter = atLeast
 		wc.lastTime = now
 	}
+}
+
+// —————
+// windowHistory remembers the CCVal window counter values of packets sent in the recent
+// past, so that it can answer queries that map the sequence number of a past outgoing
+// packet to its window counter value.
+// TODO: Use circular arithmetic on sequence numbers
+type windowHistory struct {
+	j         int
+	history   [WindowHistoryLen]windowStart
+	lastSeqNo int64
+}
+
+type windowStart struct {
+	StartSeqNo int64
+	CCVal      byte
+}
+
+const WindowHistoryLen = 4*4*2
+
+// Init resets the windowHistory instance for new use
+func (t *windowHistory) Init() {
+	t.j = 0
+	for i, _ := range t.history {
+		t.history[i] = windowStart{}
+	}
+	t.lastSeqNo = 0
+}
+
+// Add records that the window counter has changed to ccval at a packet with sequence number
+// startSeqNo
+func (t *windowHistory) Add(startSeqNo int64, ccval byte) {
+	if startSeqNo <= t.lastSeqNo {
+		panic("non-increasing sequence number")
+	}
+	t.history[t.j] = windowStart{startSeqNo, ccval}
+	t.lastSeqNo = startSeqNo
+	t.j = (t.j+1) % WindowHistoryLen
+}
+
+// Lookup returns the window counter value of the requested sequence number if it is
+// recoverable, otherwise ok equals false
+func (t *windowHistory) Lookup(seqNo int64) (ccval byte, ok bool) {
+	// The seqNo argument comes from an AckNo in a feedback packet. DCCP checks ensure that this
+	// number is bigger than the initial sequence number. Therefore Lookup can only be called
+	// after at least one call to Add. Consequently, the only case in which the history does not
+	// have the ccval of the seqNo is if the history has moved past it. This corresponds to the
+	// least recent entry in the history having a StartSeqNo greater than seqNo.
+	l := len(t.history)
+	if t.history[t.j % l].StartSeqNo > seqNo {
+		return 0, false
+	}
+	for i := 0; i < l; i++ {
+		w := t.history[(t.j+i) % l]
+		if w.StartSeqNo > seqNo {
+			break
+		}
+		ccval = w.CCVal
+	}
+	return ccval, true
 }
