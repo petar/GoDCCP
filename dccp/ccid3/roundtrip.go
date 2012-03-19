c95c0208039b680ea4a2eefad1875c688e372acb
Petar Maymounkov <petarm@gmail.com>
2012-03-18 21:28:09 -0400

    retiring receiver rtt estimator algorithm

--- dccp/ccid3/roundtrip.go
+++ dccp/ccid3/roundtrip.go
@@ -10,142 +10,10 @@ import (
 	"github.com/petar/GoDCCP/dccp"
 )
 
-// receiverRoundtripEstimator is a data structure that estimates the RTT at the receiver end.
-// It's function is described in RFC 4342, towards the end of Section 8.1.
-//
-// TOOD: Because of the necessary constraint that measurements only come from packet pairs
-// whose CCVals differ by at most 4, this procedure does not work when the inter-packet
-// sending times are significantly greater than the RTT, resulting in packet pairs whose
-// CCVals differ by 5.  Explicit RTT measurement techniques, such as Timestamp and Timestamp
-// Echo, should be used in this case.
-type receiverRoundtripEstimator struct {
-	logger *dccp.Logger
-
-	// rtt equals the latest RTT estimate, or 0 otherwise
-	rtt int64
-
-	// rttTime is the time when rtt was estimated
-	rttTime int64
-
-	// ccvalNow is what we believe is the value of the current window.
-	// A value of CCValNil means no value.
-	ccvalNow byte
-
-	// ccvalTime[i] is the time when the first packet with CCVal=i was received.
-	// A value of 0 indicates that no packet with this CCVal has been received yet.
-	ccvalTime [WindowCounterMod]int64
-}
-
-// Init initializes the RTT estimator algorithm
-func (t *receiverRoundtripEstimator) Init(logger *dccp.Logger) {
-	t.logger = logger
-	t.rtt = 0
-	t.rttTime = 0
-	t.ccvalNow = WindowCounterNil
-	for i, _ := range t.ccvalTime {
-		t.ccvalTime[i] = 0
-	}
-}
-
-// String returns the contents of the received ccvals history
-func (t *receiverRoundtripEstimator) String() string {
-	var w bytes.Buffer
-	if t.ccvalNow == CCValNil {
-		return "[]"
-	}
-	w.WriteString("[")
-	for i := byte(0); i < WindowCounterMod; i++ {
-		fmt.Fprintf(&w, "%d:%d,", 
-			(t.ccvalNow+i+1) % WindowCounterMod, 
-			t.ccvalTime[(t.ccvalNow+i+1) % WindowCounterMod])
-	}
-	w.WriteString("]")
-	return string(w.Bytes())
-}
-
-// receiver calls OnRead every time a packet is received
-func (t *receiverRoundtripEstimator) OnRead(seqno int64, ccval byte, now int64) {
-	ccval = ccval % WindowCounterMod // Safety
-
-	// Update the received ccval history
-	//
-	// XXX: The following algorithm will produce undesired results, if
-	// packet reordering switch the order of two consequtive packets with
-	// different ccvals. Ensure that re-ordering is not seen at this level.
-	// Implement re-ordering buffer at higher level that drops packets coming
-	// too late out of order.
-
-	// If this is the first received packet, or the ccval has wrapped around, ...
-	//
-	// The test for wrap around, lessWindowCounterMod(ccval, t.ccvalNow), may
-	// produce both false positves and false negatives in some circumstances.
-	// TODO(petar): Describe how both cases occur and what are the consequences.
-	if isNilWindowCounter(t.ccvalNow) || lessWindowCounterMod(ccval, t.ccvalNow) {
-		t.Init(t.logger)
-	} else {
-		c := (t.ccvalNow+1) % WindowCounterMod
-		for lessWindowCounterMod(c, ccval); i++ {
-			t.ccvalTime[c % WindowCounterMod] = 0
-			c = (c+1) % WindowCounterMod
-		}
-	}
-	t.ccvalNow = ccval
-	t.ccvalTime[ccval] = now
-
-	t.calcCounterValueBasedRTT()
-}
-
-// calcCounterValueBasedRTT calculates RTT based on CCVal timing.
-// This receiver-side approximation avoids direct measurement, while essentially trying to
-// approximate the sender's opinion of the RTT.
-//
-// XXX: Because of the necessary constraint that measurements only come from packet pairs
-// whose CCVals differ by at most 4, this procedure does not work when the inter-packet
-// sending times are significantly greater than the RTT, resulting in packet pairs whose
-// CCVals differ by 5.  Explicit RTT measurement techniques, such as Timestamp and Timestamp
-// Echo, should be used in that case. (End of Section 8.1, RFC 4243)
-func (t *receiverRoundtripEstimator) calcCounterValueBasedRTT() {
-	if isNilWindowCounter(t.ccvalNow) || t.ccvalTime[t.ccvalNow] == 0 {
-		t.logger.E("r", "rrtt-now", "rRTT no current ccval")
-		return
-	}
-	t0 := t.ccvalTime[t.ccvalNow]
-	var t1 int64
-	var q byte
-	k := (t.ccvalNow + (WindowCounterMod-4)) % WindowCounterMod  // Equals (ccvalNow-4) mod WindowCounterMod
-	switch {
-	case t.ccvalTime[k] != 0:
-		t1 = t.ccvalTime[k]
-		q = 4
-	case t.ccvalTime[(k+1) % WindowCounterMod] != 0:
-		t1 = t.ccvalTime[(k+1) % WindowCounterMod]
-		q = 3
-	case t.ccvalTime[(k+2) % WindowCounterMod] != 0:
-		t1 = t.ccvalTime[(k+2) % WindowCounterMod]
-		q = 2
-	}
-	if t1 == 0 {
-		t.logger.E("r", "rrtt-hist", "rRTT deficient history")
-		return
-	}
-
-	t.rtt = (4 * (t0-t1)) / int64(q)
-	t.rttTime = t0
-}
-
-// RTT returns the best available estimate of the round-trip time
-func (t *receiverRoundtripEstimator) RTT(now int64) (rtt int64, estimated bool) {
-	if t.rtt != 0 &&  now - t.rttTime < 1e9 {
-		return t.rtt, true
-	}
-	return 1e9, false
-}
-
-
 // senderRoundtripEstimator is a data structure that estimates the RTT at the sender end.
 type senderRoundtripEstimator struct {
 	estimate int64
-	k        int				// The index of the next history cell to write in
+	k        int					// The index of the next history cell to write in
 	history  [SenderRoundtripHistoryLen]sendTime	// Circular array, recording departure times of last few packets
 }
 
@@ -246,4 +114,44 @@ func (t *senderRoundtripEstimator) RTT() (rtt int64, estimated bool) {
 // HasRTT returns true if senderRoundtripEstimator has enough sample data for an estimate
 func (t *senderRoundtripEstimator) HasRTT() bool {
 	return t.estimate > 0
+}
+
+// receiverRoundtripEstimator is a data structure that estimates the RTT at the receiver end.
+// Instead of using the less precise algorithm described in RFC 4342, towards the end of Section
+// 8.1, we simply record the RTT estimate calculated at the sender and communicated via an option.
+type receiverRoundtripEstimator struct {
+	logger *dccp.Logger
+
+	// rtt equals the latest RTT estimate, or 0 otherwise
+	rtt int64
+
+	// rttTime is the time when RTT estimate was received
+	rttTime int64
+}
+
+// Init initializes the RTT estimator
+func (t *receiverRoundtripEstimator) Init(logger *dccp.Logger) {
+	t.logger = logger
+	t.rtt = 0
+	t.rttTime = 0
+}
+
+// String returns the contents of the received ccvals history
+func (t *receiverRoundtripEstimator) String() string {
+	var w bytes.Buffer
+	fmt.Fprintf(&w, "RTT=%s ns", dccp.Nstoa(t.rtt))
+	return string(w.Bytes())
+}
+
+// receiver calls OnRead every time a packet is received
+func (t *receiverRoundtripEstimator) OnRead(seqno int64, ccval byte, now int64) {
+	??
+}
+
+// RTT returns the best available estimate of the round-trip time
+func (t *receiverRoundtripEstimator) RTT(now int64) (rtt int64, estimated bool) {
+	if t.rtt != 0 &&  now - t.rttTime < 1e9 {
+		return t.rtt, true
+	}
+	return dccp.RoundtripDefault, false
 }
