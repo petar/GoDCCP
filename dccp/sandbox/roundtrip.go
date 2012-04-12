c44edab6272845c356de692be81346cf13670bf4
Petar Maymounkov <petarm@gmail.com>
2012-04-11 22:35:56 -0400

    add independent one-directional trip time measurements in sandbox

--- dccp/sandbox/roundtrip.go
+++ dccp/sandbox/roundtrip.go
@@ -5,7 +5,10 @@
 package sandbox
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
+	"time"
 	"github.com/petar/GoDCCP/dccp"
 )
 
@@ -13,19 +16,71 @@ import (
 // emitted from the RTT test and performs various checks.
 type roundtripReducer struct {
 	t *testing.T
+	leftClient map[int64]int64  // SeqNo —> Time left client
+	leftServer map[int64]int64  // SeqNo —> Time left server
+	clientToServer Moment
+	serverToClient Moment
 }
 
 func newRoundtripReducer(t *testing.T) *roundtripReducer {
-	return &roundtripReducer{t}
+	x := &roundtripReducer{
+		t: t,
+		leftClient: make(map[int64]int64),
+		leftServer: make(map[int64]int64),
+	}
+	x.clientToServer.Init()
+	x.serverToClient.Init()
+	return x
 }
 
-func (t *roundtripReducer) Write(r *dccp.LogRecord) {
+func (x *roundtripReducer) Write(r *dccp.LogRecord) {
+	now := time.Now().UnixNano()
+	switch r.Event {
+	case dccp.EventWrite:
+		switch r.Labels[0] {
+		case "client":
+			x.leftClient[r.SeqNo] = now
+		case "server":
+			x.leftServer[r.SeqNo] = now
+		}
+	case dccp.EventRead:
+		switch r.Labels[0] {
+		case "client":
+			left, ok := x.leftServer[r.SeqNo]
+			if !ok {
+				fmt.Printf("client read, no server write, seqno=%06x\n", r.SeqNo)
+			} else {
+				x.serverToClient.Add(float64(now - left))
+			}
+		case "server":
+			left, ok := x.leftClient[r.SeqNo]
+			if !ok {
+				fmt.Printf("server read, no client write, seqno=%06x\n", r.SeqNo)
+			} else {
+				x.clientToServer.Add(float64(now - left))
+			}
+		}
+	case dccp.EventDrop:
+		delete(x.leftClient, r.SeqNo)
+		delete(x.leftServer, r.SeqNo)
+	}
 }
 
-func (t *roundtripReducer) Sync() error { 
+func (x *roundtripReducer) Sync() error { 
 	return nil 
 }
 
-func (t *roundtripReducer) Close() error { 
+func (x *roundtripReducer) String() string {
+	var w bytes.Buffer
+	fmt.Fprintf(&w, "c—>s %0.1f/%0.1f ms, c<—s %0.1f/%0.1f, unclaimed lc:%d ls:%d", 
+		x.clientToServer.Average()/1e6, x.clientToServer.StdDev()/1e6,
+		x.serverToClient.Average()/1e6, x.serverToClient.StdDev()/1e6,
+		len(x.leftClient), len(x.leftServer),
+	)
+	return string(w.Bytes())
+}
+
+func (x *roundtripReducer) Close() error { 
+	fmt.Println(x.String())
 	return nil 
 }
