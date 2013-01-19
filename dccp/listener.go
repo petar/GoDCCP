341996ff13752d80bacf7f83c40ae88c11944607
Petar Maymounkov <petar@tumblr.com>
2013-01-19 16:10:26 +0200

    copyright notice

--- dccp/listener.go
+++ dccp/listener.go
@@ -1,4 +1,4 @@
-// Copyright 2011 GoDCCP Authors. All rights reserved.
+// Copyright 2011-2013 GoDCCP Authors. All rights reserved.
 // Use of this source code is governed by a 
 // license that can be found in the LICENSE file.
 
